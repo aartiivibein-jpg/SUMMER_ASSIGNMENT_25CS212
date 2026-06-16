df2ae37788f426878fddfa98ce06d0dc9e749ad7
Aarti <aartii.vibein@gmail.com>
2026-06-16 22:27:10 +0530

    added DAY12

---
+++ DAY12/perfect.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class perfect {
+
+    static boolean isPerfect(int num) {
+        int sum = 0;
+
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum = sum + i;
+            }
+        }
+
+        return sum == num;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (isPerfect(num))
+            System.out.println("Perfect Number");
+        else
+            System.out.println("Not a Perfect Number");
+
+        sc.close();
+    }
+}
