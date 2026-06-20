930943dbe9328063610734f0c84694d9e9ab9645
Aarti <aartii.vibein@gmail.com>
2026-06-21 00:30:00 +0530

    added DAY17

---
+++ DAY17/merge.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class merge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {4, 5, 6};
+
+        int[] merge = new int[arr1.length + arr2.length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            merge[i] = arr1[i];
+        }
+
+        for (int i = 0; i < arr2.length; i++) {
+            merge[arr1.length + i] = arr2[i];
+        }
+
+        System.out.println("Merged Array:");
+
+        for (int i = 0; i < merge.length; i++) {
+            System.out.print(merge[i] + " ");
+        }
+    }
+}
