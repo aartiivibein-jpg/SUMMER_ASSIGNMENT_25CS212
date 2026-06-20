e2d1b0e8a899a5e9baaf786f234e8c0110d4f098
Aarti <aartii.vibein@gmail.com>
2026-06-21 00:31:20 +0530

    added DAY18

---
+++ DAY18/descending.java
@@ -0,0 +1,24 @@
+public class descending  {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 2, 8, 1, 9};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Array in Descending Order:");
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
